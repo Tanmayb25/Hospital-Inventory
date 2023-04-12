f5b8ff81705f5e18adbd37ea9d67ca0608573cab
Tanmayb25 <tanmay.22111091@viit.ac.in>
2023-04-12 10:41:31 +0530

    table added

--- src/Pages/med.js
+++ src/Pages/med.js
@@ -4,40 +4,35 @@ import Table from 'react-bootstrap/Table';
 function med() {
   return (
     <div>
-     
-
-
-    <Table responsive>
+    <Table striped>
       <thead>
         <tr>
           <th>#</th>
-          {Array.from({ length: 2 }).map((_, index) => (
-            <th key={index}>Name</th>
-          ))}
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>1</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
         </tr>
         <tr>
           <td>2</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
         </tr>
         <tr>
           <td>3</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
         </tr>
       </tbody>
     </Table>
-  
 
 
     </div>
